0ed222587334a59443d9a8019ac661cfc1f947de
ngoyenkhanh29597 <ngoyenkhanh29597@gmail.com>
2023-05-15 17:55:17 +0700

    Operator

---
+++ Buoi 01/Operator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Operator
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                float width;
+                float height; 
+                Console.Write("Enter width of rectangle: ");
+                width = float.Parse(Console.ReadLine());
+                Console.Write("Enter height of rectangle: ");
+                height = float.Parse(Console.ReadLine());
+                float area = width * height;
+                Console.WriteLine("The area is: " + area);
+            }
+        }
+    }
