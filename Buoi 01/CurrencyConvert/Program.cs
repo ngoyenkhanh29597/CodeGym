bbb31e4b3208e35c7671c3b52f96aa2b88d3501c
ngoyenkhanh29597 <ngoyenkhanh29597@gmail.com>
2023-05-15 18:56:50 +0700

    CurrencyConvert

---
+++ Buoi 01/CurrencyConvert/Program.cs
@@ -0,0 +1,5 @@
+﻿float _USD;
+Console.Write("Enter the value of USD currency: ");
+_USD = float.Parse(Console.ReadLine());
+float _VND = 23459 * _USD;
+Console.WriteLine("Value of VND currency is: " + _VND);
