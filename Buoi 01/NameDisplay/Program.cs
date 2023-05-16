807dd9e10d3b2c1cc1ba7e7d24a4acfd2cde4094
ngoyenkhanh29597 <ngoyenkhanh29597@gmail.com>
2023-05-16 18:53:05 +0700

    Buoi02

--- Buoi 01/NameDisplay/Program.cs
+++ Buoi 01/NameDisplay/Program.cs
@@ -1,5 +1,12 @@
-﻿Console.WriteLine("What's your name: ");
-
-string myName = Console.ReadLine();
-
-Console.WriteLine("Hello: " + myName);
+﻿//Khai báo biến lưu trữ dữ liệu
+string name;
+int birthyear;
+//Nhập dữ liệu
+Console.Write("What's your name? ");
+name = Console.ReadLine();
+Console.WriteLine("What's your birthyear? ");
+birthyear = int.Parse(Console.Read());
+//Tính tuổi dựa vào năm sinh
+int age = DateTime.Now.Year - birthyear;
+Console.WriteLine("Hello: " + name + ", your age is " + age );
+//C# bắt đầu là STRING kết thúc là STRING (toString())
---
+++ Buoi 02/BirthdayPredict/BirthdayPredict/BirthdayPredict/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace BirthdayPre
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your date of birth: ");
+        }
+    }
+}
+
+
