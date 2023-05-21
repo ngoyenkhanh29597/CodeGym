d0aa1c1988ab890cbe909fc8a27a157d2c0c0e00
ngoyenkhanh29597 <ngoyenkhanh29597@gmail.com>
2023-05-22 01:27:08 +0700

    Menu

---
+++ Buoi 04/Menu/Menu/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace Menu;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int option;
+        do
+        {
+            Console.WriteLine("Drawing Menu");
+            Console.WriteLine("1. Draw the triangle");
+            Console.WriteLine("2. Draw the square");
+            Console.WriteLine("3. Draw the rectangle");
+            Console.WriteLine("0. Exit");
+            Console.Write("Enter your choice: ");
+            option = Int32.Parse(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine("Draw the triangle");
+                    Console.WriteLine("******");
+                    Console.WriteLine("*****");
+                    Console.WriteLine("****");
+                    Console.WriteLine("***");
+                    Console.WriteLine("**");
+                    Console.WriteLine("*");
+                    break;
+                case 2:
+                    Console.WriteLine("Draw the square");
+                    Console.WriteLine("* * * * * *");
+                    Console.WriteLine("* * * * * *");
+                    Console.WriteLine("* * * * * *");
+                    Console.WriteLine("* * * * * *");
+                    Console.WriteLine("* * * * * *");
+                    Console.WriteLine("* * * * * *");
+                    break;
+                case 3:
+                    Console.WriteLine("Draw the rectangle");
+                    Console.WriteLine("* * * * * *");
+                    Console.WriteLine("* * * * * *");
+                    Console.WriteLine("* * * * * *");
+                    break;
+                case 0:
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine("No choice!");
+                    break;
+            }
+        } while (option != 0);
+
+    }
+}
+
