9021a19da7587f307507b36223a972e677ee4260
ngoyenkhanh29597 <ngoyenkhanh29597@gmail.com>
2023-05-22 08:51:32 +0700

    Check Prime

---
+++ Buoi 04/PrimeNumber/PrimeNumber/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace PrimeNumber;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int num;
+        int count = 0;
+        Console.WriteLine("Indentify Prime number program: ");
+        Console.Write("Enter a number: ");
+        num = int.Parse(Console.ReadLine());
+        for (int i = 1; i <= Math.Sqrt(num); i++)
+        {
+            if (num % i == 0)
+                count++;
+            {
+                if (count > 2)
+                {
+                    Console.WriteLine(num + " is not a prime number");
+                    break;
+                }
+            }
+        }
+        if (count <= 2) Console.WriteLine(num + " is a prime number");
+        Console.ReadKey();
+    }
+}
+
