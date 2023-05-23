6edd8e462b3f10244fe7b84e4b5ebba5aa3345a8
ngoyenkhanh29597 <ngoyenkhanh29597@gmail.com>
2023-05-24 01:10:37 +0700

    Sum of array

---
+++ Buoi 05/Array/Array/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Array;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] b = { 8, 9, 4, 10, 6 };
+        //[0; len-1]
+        for (int i = 0; i < b.Length; i++)
+            Console.Write(b[i] + " ");
+        Console.WriteLine();
+
+        Console.WriteLine("Dung lenh for each");
+        foreach (int i in b) //nhanh hon rat nhieu so voi for
+            Console.Write(i + " ");
+        Console.WriteLine();
+
+        Console.WriteLine("Viet gon hon:");
+        Console.WriteLine(string.Join(" | ",b));
+
+
+        Console.ReadKey();
+
+        /***
+        int[] a = new int[5];
+        int[] b = { 8, 9, 4 };
+		//int c  = new int[5] { 5, 2, 4 };
+		int[] d  = new int[5] { 5, 2, 4, 7, 1 };
+		int[] e  = new int[] { 3, 4, 7 };
+        Console.ReadKey();
+        //Check phần tử đầu tiên a[0] = 0, b[0],..,         b[0] = 8
+        //Check phần tử thứ 5 a[5] = error, b[5] = error,   b[5] = error
+        //Check phần tử thứ 7 a[7] = errpr. b[7] = error,   b[7] = error
+        ***/
+    }
+}
+
---
+++ Buoi 05/SumofArray/SumofArray/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace SumofArray;
+class Program
+{
+    static void Main(string[] args)
+    {
+        TinhTongCacPhanTuMang();
+    }
+
+    private static void TinhTongCacPhanTuManglist()
+    {
+        List<int> a = new List<int>();
+        Console.Write("So luong phan tu can nhap: ");
+
+    }
+
+    private static void TinhTongCacPhanTuMang()
+    {
+        ////Cach 1: dung for nhap vao mang
+        //Khai bao mang
+        int[] a = new int[10];
+        Console.Write("So luong phan tu can nhap: ");
+        int n = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Nhap gia tri cac phan tu: ");
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write("Phan tu thu [" + i + "]=");
+
+            //Phan tu thu [0] = ???
+            Console.Write("Phan tu thu [" + i + "]=");
+            //Luu gia tri vo mang
+            a[i] = int.Parse(Console.ReadLine());
+        }
+        Console.WriteLine();
+        Console.WriteLine("Xuat du lieu vua nhap:");
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write(a[i] + " ");
+        }
+        Console.WriteLine();
+        int tong = 0;
+        for (int i = 0; i < n; i++)
+            tong += a[i];
+                Console.WriteLine("Tong gia tri cua mang la: " + tong);
+        Console.ReadKey();
+            
+    }
+}
+        
+
