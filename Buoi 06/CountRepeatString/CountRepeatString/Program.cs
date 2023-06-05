80a1b7789ca86ee4af7e61cd1dade1d3ecef5d23
ngoyenkhanh29597 <ngoyenkhanh29597@gmail.com>
2023-06-05 14:43:12 +0700

    Letter counting

---
+++ Buoi 06/CountRepeatString/CountRepeatString/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace CountRepeatString;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string input;
+        Console.Write("Enter a string: ");
+        input = Console.ReadLine();
+        Console.WriteLine("The input string: " + input);
+
+        Console.Write("Enter the letter that need to search: ");
+        //char letter = Console.ReadKey().KeyChar;
+        char letter = char.Parse(Console.ReadLine());
+
+        Console.WriteLine("Number of repeatitions of the letter " + letter + " in string " + input + " is " + countinString(input, letter));
+
+
+        Console.ReadKey();
+    }
+    static int countinString(string inputString, char letter)
+    {
+        int count = 0;
+        foreach (char letter_for in inputString)
+        {
+            if (letter_for == letter)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+}
+
