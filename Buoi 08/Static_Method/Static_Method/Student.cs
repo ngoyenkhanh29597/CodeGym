3f911767ba66c5eda70590c1039351820a7c7ed9
ngoyenkhanh29597 <ngoyenkhanh29597@gmail.com>
2023-06-09 16:24:32 +0700

    Static

---
+++ Buoi 08/Static_Method/Static_Method/Student.cs
@@ -0,0 +1,26 @@
+﻿public class Student
+{
+    private int ID;
+    private string name;
+    private static string college = "BBDIT";
+
+    //constructor to initialize the variable
+    public Student(int id, string n)
+    {
+        ID = id;
+        name = n;
+    }
+
+    //static method to change the value of static variable
+
+    public static void Change()
+    {
+        college = "International University";
+    }
+
+    //method to display values
+    public void Display()
+    {
+        Console.WriteLine(ID + " " + name + " " + college);
+    }
+}
