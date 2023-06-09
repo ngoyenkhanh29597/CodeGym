c84e38a4fdab7775afd39284f58f3387eae6a4a6
ngoyenkhanh29597 <ngoyenkhanh29597@gmail.com>
2023-06-09 14:14:57 +0700

    Cat

---
+++ Buoi 08/Animal_Class_Practice/Animal_Class_Practice/Program.cs
@@ -0,0 +1,12 @@
+﻿using Animal;
+
+namespace Animal_Class_Practice;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Cat cat = new Cat("30kg", "2.6", "Mina");
+        cat.PrintInfo();
+    }
+}
+
