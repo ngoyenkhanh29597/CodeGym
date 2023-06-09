b469742c828d4d4786727cba3f74d5fd6d916ab5
ngoyenkhanh29597 <ngoyenkhanh29597@gmail.com>
2023-06-09 17:31:10 +0700

    Sum in text

---
+++ Buoi 11/Sum_in_txt/SumInText/SumInText/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace SumInText;
+class ReadTextFileExample
+{
+    static void Main(string[] args)
+    {
+        Console.Write("File path: ");
+        string path = Console.ReadLine();
+        ReadTextFileExample example = new ReadTextFileExample();
+        example.ReadTextFile(path);
+    }
+
+    void ReadTextFile(string filePath)
+    {
+        try
+        {
+            FileInfo file = new FileInfo(filePath);
+            if (!file.Exists)
+            {
+                throw new FileNotFoundException();
+            }
+
+            StreamReader reader = new StreamReader(filePath);
+            string line = "";
+            int sum = 0;
+            while ((line = reader.ReadLine()) != null)
+            {
+                Console.WriteLine(line);
+                sum += int.Parse(line);
+            }
+            reader.Close();
+            Console.WriteLine("Total: " + sum);
+        }
+        catch (System.Exception)
+        {
+            Console.Error.WriteLine("File not found or invalid content");
+        }
+    }
+}
+
