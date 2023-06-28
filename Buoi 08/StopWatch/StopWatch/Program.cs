6561926733cc81c0545ca03dae0b3cc125534283
ngoyenkhanh29597 <ngoyenkhanh29597@gmail.com>
2023-06-28 09:40:32 +0700

    Not done

---
+++ Buoi 08/StopWatch/StopWatch/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace StopWatch;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Current Date and Time is : ");
+        DateTime now = DateTime.Now;
+        Console.WriteLine(now.ToString("dd/MM/yyyy HH:mm:ss.fffffff"));
+        Console.ReadLine();`
+    }
+}
+
